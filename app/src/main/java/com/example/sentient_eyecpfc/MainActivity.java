fd3f1e90dcfc78fec30c642de434a810d321a975
Matvey <mathook00@gmail.com>
2019-04-17 23:29:21 +0300

    Fix

--- app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
+++ app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.sentient_eyecpfc;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
+
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -14,25 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton scannerImageButton;
     DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
-    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scannerImageButton = findViewById(R.id.scanner_image_button);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        toolbar = findViewById(R.id.toolbar);
-        navigationView = findViewById(R.id.nav_view);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         scannerImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +40,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentLoadScannerActivity);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
