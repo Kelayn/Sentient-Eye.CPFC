81405e5eb5a5d43fde28d0550fcfe00d44530b0d
Matvey <mathook00@gmail.com>
2019-04-17 15:20:41 +0300

    Added SideBar. Added properties to gradle, new action for sidebar; Closing when installing on phone, but in builing correctly.

--- app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
+++ app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.sentient_eyecpfc;
 
 import android.content.Intent;
-import android.media.Image;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton scannerImageButton;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scannerImageButton = (ImageButton) findViewById(R.id.scanner_image_button);
+        scannerImageButton = findViewById(R.id.scanner_image_button);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
+
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
         scannerImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
