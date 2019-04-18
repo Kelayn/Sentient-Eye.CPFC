1be714e697d04050de674fd55d5e3d2363983652
Matvey <mathook00@gmail.com>
2019-04-18 17:33:00 +0300

    Fixed problems with crashes, added working SideBar with 7 different Fragments and Layouts. Added all fragments to MainActivity and property for rotatation phote/display.

--- app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
+++ app/src/main/java/com/example/sentient_eyecpfc/MainActivity.java
@@ -1,20 +1,17 @@
 package com.example.sentient_eyecpfc;
 
-import android.content.Intent;
-
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    ImageButton scannerImageButton;
     DrawerLayout drawerLayout;
 
     @Override
@@ -26,20 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
                 toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        scannerImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View e) {
-                Intent intentLoadScannerActivity =
-                        new Intent(MainActivity.this, ScannerActivity.class);
-                startActivity(intentLoadScannerActivity);
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ProfileFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_profile);
+        }
     }
 
     @Override
@@ -49,5 +45,42 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_statistic:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new StatisticFragment()).commit();
+                break;
+            case R.id.nav_scanner:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ScannerFragment()).commit();
+                break;
+            case R.id.nav_food:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FoodFragment()).commit();
+                break;
+            case R.id.nav_plan_fitnes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FitnessPlanFragment()).commit();
+                break;
+            case R.id.nav_calculator:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalculatorFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
