f8f9f81f254b890420badd0574c6776564e4e95f
Andrew Arakelyan <andrey.arakelyan@bask.ws>
2019-05-27 18:14:45 +0300

    SailentZzZ > [feature/Barcode-Scanner] < barcode added

--- app/src/main/java/com/example/sentient_eyecpfc/ScannerFragment.java
+++ app/src/main/java/com/example/sentient_eyecpfc/ScannerFragment.java
@@ -1,11 +1,18 @@
 package com.example.sentient_eyecpfc;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,38 +28,42 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 import java.util.Objects;
 
 public class ScannerFragment extends Fragment {
+    public static TextView textView;
+    Button btn_scan;
+    private static final String TAG = "Acces";
+    private static final int REQUEST_CODE = 1;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_barcode_detect, container, false);
 
-        TextView txtView = view.findViewById(R.id.txtContent);
-        Button btn_scan = view.findViewById(R.id.button_scan);
-
-        ImageView scanImageView = view.findViewById(R.id.img_scan);
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.puppy);
-        scanImageView.setImageBitmap(bitmap);
-
-        BarcodeDetector detector = new BarcodeDetector.Builder(getActivity())
-                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
-
-        if (!detector.isOperational()) {
-            txtView.setText("Couldn't set up detector!");
-            return null;
-        }
-
-        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
-
-        Barcode thisBarcode = barcodes.valueAt(0);
-        txtView.setText(thisBarcode.rawValue);
+        textView = view.findViewById(R.id.textView);
+        btn_scan = view.findViewById(R.id.btn_scan);
 
         btn_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                verify();
             }
         });
 
         return view;
+    }
+
+    private void verify() {
+        Log.d(TAG, "asking user for permission");
+        String[] permission = {Manifest.permission.CAMERA};
+
+        if (ContextCompat.checkSelfPermission(this.getContext(), permission[0]) == PackageManager.PERMISSION_GRANTED) {
+            startActivity(new Intent(getContext(), OpenCameraScanActivity.class));
+        } else {
+            requestPermissions(permission, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verify();
     }
 }
