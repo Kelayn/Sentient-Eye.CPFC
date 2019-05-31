a438bc555c47e0d73584442a694fe40e533975da
Andrew Arakelyan <andrey.arakelyan@bask.ws>
2019-06-01 01:58:10 +0300

    Added gist for calories and DB cap

--- app/src/main/java/com/example/sentient_eyecpfc/StatisticFragment.java
+++ app/src/main/java/com/example/sentient_eyecpfc/StatisticFragment.java
@@ -1,5 +1,6 @@
 package com.example.sentient_eyecpfc;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,25 +9,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sentient_eyecpfc.Data.DBProduct;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.List;
+
 
 public class StatisticFragment extends Fragment {
+    private List<DBProduct> mProducts;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistic, container, false);
-        GraphView graph = view.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+        GraphView calGraph = view.findViewById(R.id.caloriesGist);
+        // mProducts = ListFromDb();
+        DataPoint[] points = new DataPoint[mProducts.size()];
+        for (int day = 0; day < mProducts.size(); day++) {
+            //points[day] = new DataPoint(day, mProducts[day].getCalories()); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        }
+        BarGraphSeries<DataPoint> calorieGist = new BarGraphSeries<>(points);
+
+
+        calorieGist.setColor(Color.argb(100, 34, 139, 34));
+        calGraph.addSeries(calorieGist);
+
+        // title
+        calGraph.setTitle("Calories gist");
+
+        calGraph.getGridLabelRenderer().setVerticalAxisTitle("Amount");
+        calGraph.getGridLabelRenderer().setHorizontalAxisTitle("Date");
+
+        // set manual X bounds
+        calGraph.getViewport().setXAxisBoundsManual(true);
+        calGraph.getViewport().setMinX(0);
+        calGraph.getViewport().setMaxX(10);
+
+        // enable scrolling
+        calGraph.getViewport().setScrollable(true);
+
         return view;
     }
 }
